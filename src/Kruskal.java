292e9c5f6635d9df15db42cd59f1bd3af77ef93f
hongsu87 <hongsubakgame@naver.com>
2021-03-31 11:54:33 +0900

    .

---
+++ src/Kruskal.java
@@ -0,0 +1,5 @@
+public class Kruskal implements MinimumSpanningTree{
+    public void convert(int[][] A) {
+
+    }
+}
---
+++ src/Prim.java
@@ -0,0 +1,5 @@
+public class Prim {
+    public void convert(int [][] A) {
+
+    }
+}
